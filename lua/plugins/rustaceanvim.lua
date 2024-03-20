e67e54bc97f3b058f8d9f85a37de76f28bbe3d33
Guillaume Lagrange <guillaume@glagrange.eu>
2024-03-20 03:06:39 +0100

    rust

---
+++ lua/plugins/rustaceanvim.lua
@@ -0,0 +1,21 @@
+vim.g.rustaceanvim = {
+  -- Plugin configuration
+  tools = {},
+  -- LSP configuration
+  server = {
+    load_vscode_settings = true,
+    default_settings = {
+      ['rust-analyzer'] = {},
+    },
+  },
+  -- DAP configuration
+  dap = {},
+}
+
+return {
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    ft = { 'rust' },
+  },
+}
