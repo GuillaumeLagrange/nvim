47d0700c3faba282558520c7feabc7da861cd6b1
Guillaume Lagrange <guillaume@glagrange.eu>
2024-11-21 23:11:02 +0100

    feat: enable prettier formatting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -73,12 +73,11 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         python = { 'isort', 'black' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javastript = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
       },
     },
   },
